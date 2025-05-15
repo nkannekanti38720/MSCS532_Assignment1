ea874faf92543658ce86e41cf9abcab3d7c88994
raj <rronta526@students.campbellsville.edu>
2025-05-15 00:24:56 -0600

    changed code

--- insertion sort.py
+++ insertion sort.py
@@ -1,11 +1,11 @@
 def insertion_sort_desc(arr):
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] < key:
-            arr[j + 1] = arr[j]
+        j = i
+        while j > 0 and arr[j-1] < key:
+            arr[j] = arr[j-1]
             j -= 1
-        arr[j + 1] = key
+        arr[j] = key
 
 if __name__ == "__main__":
     arr = [5, 2, 9, 1, 5, 6]
